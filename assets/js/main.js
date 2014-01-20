4eebeb83100356e92f8d9baf9d8846508a6c01f5
Jasmeet Oberai <jasoberai@gmail.com>
2014-01-20 04:16:43 -0500

    added embedded pdf

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', setPdf, false);
+
+function setPdf(){
+  var pdf = document.getElementById("pdf-resume");
+
+  if (pdf){
+    pdf.style.height = pdf.offsetWidth * 13 / 10+ 'px';
+  }
+  else{
+    return false;
+  }
+
+  window.onresize = function(event) {
+    setPdf();
+  }
+}
